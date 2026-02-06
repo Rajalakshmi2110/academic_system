a97fa6e06384a4c821704bec8a6fafa9938cfeb2
Rajalakshmi R <rathrajy@amazon.com>
2026-02-07 00:22:09 +0530

    Clean up: update .gitignore, move syllabus file, remove duplicate scripts

--- start_backend.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "🚀 Backend starting on http://localhost:5000"
-cd backend
-python app.py
--- start_frontend.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "🎨 Frontend starting on http://localhost:3000"
-cd frontend
-npm install
-npm start
